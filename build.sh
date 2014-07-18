e0ce0c54c764aee416ad86948b51fe1f6b348e6a
picomancer <pico-ghpub-wgve433t-onlyham@picomancer.com>
2014-07-18 03:45:17 -0400

    build.sh:  Add set -e, set -x

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+set -x
 mkdir -p bin
 (cd src && mcs *.cs -r:../dep/OpenTK.dll -r:System.Drawing -out:../bin/lifecube.exe)
 [ -e bin/OpenTK.dll ] || ln -s ../dep/OpenTK.dll bin/OpenTK.dll
